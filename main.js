a1548976919afd86ff4eb1f72a100e64f547712e
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-10-19 12:16:13 +0600

    rest parameter for catch large numbers of parameter

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let poorcountry = ['Bangladesh', 'srilanka', 'Nepal']
-let richcountry = ['America', 'canada', 'China', 'Itali']
-richcountry.push(poorcountry)
-console.log(richcountry)
+function show(...numbers){
+    let sum = 0;
+    for (let i of numbers)
+    {
+        sum+=i;
+    }
+    console.log(sum);
+}
+show(1,2,3,4,5,6,7,8,9)
