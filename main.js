3d9ed259c6026b1023355dc4530a94593cfbc983
mdsajedulislamsajib <mdsajedulislamsajib25@gmail.com>
2024-10-20 21:16:11 +0600

    simple function in ES6

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
-var nameArray=['kamal','sajib','jamal','sajib']
-for(let oneName of nameArray)
-{
-    console.log(oneName)
+// var billGates={
+//     shirt:true,
+//     shirtColor:"offwhite",
+//     smile:true,
+//     sweter:true,
+//     swetercolor:"gray",
+//     chosma:true
+
+// }
+// console.log(billGates['shirtColor']);
+// var billGatesPro={
+//     shirt:{
+//         color:"offWhite",
+//         Quality:"good",
+//         price:"200USD"
+//     },
+//     sweter:{
+//         color:"Gray",
+//         Quality:"good",
+//         price:"230USD",
+//         warmth:"best"
+
+//     }
+// }
+
+// console.log(billGatesPro['sweter']['price']);
+//------------------
+
+// var billGates={
+//     shirt:true,
+//     shirtColor:"offwhite",
+//     smile:true,
+//     sweter:true,
+//     swetercolor:"gray",
+//     chosma:true
+
+// }
+// if(billGates['shirtColor']=="red"){
+//     console.log("Shirt Color is Red");
+// }
+// if(billGates['shirtColor']=="blue"){
+//     console.log("Shirt Color is Blue");
+// }
+// if(billGates['shirtColor']=="offWhite"){
+//     console.log("Shirt Color is offWhite");
+// }
+
+function myName(){
+    var x=10;
+    var y=10;
+    var z=x+y;
+    console.log(z);
+
 }
