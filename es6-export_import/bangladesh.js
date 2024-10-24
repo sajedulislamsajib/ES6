74f0f7061e8c50c6cd4ffe9107cf87261c0ab259
Md.Sajedul Islam Sajib <62029300+sajedulislamsajib@users.noreply.github.com>
2024-10-24 10:19:54 +0600

    Add files via upload

---
+++ es6-export_import/bangladesh.js
@@ -0,0 +1,12 @@
+//-------------Named Export Import----------------
+
+// import { laptop,MobilePhn,Cosmetics } from "./china.js";
+// MobilePhn();
+// // console.log(laptop)
+// Cosmetics();
+
+
+//class import
+import{China} from './china.js';
+let obj =new China();
+obj.Toys()
---
+++ es6-export_import/china.js
@@ -0,0 +1,22 @@
+//-------------Named Export Import----------------
+
+
+// const laptop= "Dell Laptop";
+// function MobilePhn(){
+//     console.log("Mobile Phone from Chaina")
+// }
+// function Cosmetics(){
+//     console.log("Cosmetic from china")
+// }
+
+// export{Toys,laptop,MobilePhn,Cosmetics};
+
+
+//class imported
+class China{
+    Toys(){
+        alert("Toys Imported from china")
+    }
+}
+
+export{China}
